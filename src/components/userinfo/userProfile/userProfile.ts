ac5a1e8f1b71dbc8f784a8545534f1ee95961044
David <114586818+ZDavid73@users.noreply.github.com>
2023-11-17 12:42:08 -0500

    UserProfile

--- src/components/userinfo/userProfile/userProfile.ts
+++ src/components/userinfo/userProfile/userProfile.ts
@@ -17,24 +17,24 @@ export class UserProfile extends HTMLElement {
             link.setAttribute("href", "/src/components/userinfo/userProfile/userProfile.css")
             this.shadowRoot.appendChild(link);
 
-            const menuContainer = this.ownerDocument.createElement("div");
+            const menuContainer = (this.ownerDocument as Document).createElement("div");
             menuContainer.setAttribute("id", "menuUserProfile");
-            this.shadowRoot.appendChild(menuContainer);
-
-            const title = this.ownerDocument.createElement("h1");
+            (this.shadowRoot as ShadowRoot).appendChild(menuContainer);
+    
+            const title = (this.ownerDocument as Document).createElement("h1");
             title.innerHTML = "Nombre de usuario";
             menuContainer.appendChild(title);
-
-            const subTitle = this.ownerDocument.createElement("h2");
+    
+            const subTitle = (this.ownerDocument as Document).createElement("h2");
             subTitle.innerHTML = "@nombre_de_usuario";
             menuContainer.appendChild(subTitle);
-
-            const menuContainerimg = this.ownerDocument.createElement("div");
-            menuContainer.setAttribute("id", "menuUserProfileimg");
-            this.shadowRoot.appendChild(menuContainer);
-
-            const nameInput = this.ownerDocument.createElement("img");
-            nameInput.setAttribute("img", "/src/resources/svg/Icons/User.svg");
+    
+            const menuContainerimg = (this.ownerDocument as Document).createElement("div");
+            menuContainerimg.setAttribute("id", "menuUserProfileimg");
+            menuContainer.appendChild(menuContainerimg);
+    
+            const nameInput = (this.ownerDocument as Document).createElement("img");
+            nameInput.setAttribute("src", "/src/resources/svg/Icons/User.svg");
             menuContainerimg.appendChild(nameInput);
 
         }
